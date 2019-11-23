f19e4f27c448ffde6d6a116e5ab6666083c09e65
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-23 14:29:37 +0200

    Comment code

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// Get video tag by Id
 const video = document.getElementById("video");
 
 Promise.all([
   // Small face detector
   faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
-  // Register face parts
+  // Recognize face parts
   faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
   // Recognize face box location
   faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
@@ -11,10 +12,12 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri("./models")
 ]).then(startVideo);
 
+// Hook up web cam to video HTML element
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
     stream => {
+      // Get video from web cam and set as source for video HTML element
       video.srcObject = stream;
     },
     err => {
@@ -23,12 +26,31 @@ function startVideo() {
   );
 }
 
+// When video starts playing start recognizing faces
 video.addEventListener("play", () => {
+  // Create canvas from video
+  const canvas = faceapi.createCanvasFromMedia(video);
+  // Put canvas on page
+  document.body.append(canvas);
+  // Reize canvas over video
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  // Get all faces inside "video" every 100 ms
   setInterval(async () => {
     const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .detectAllFaces(video,
+        // Detect with tiny face API
+        new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
-      .withFaceExpressions()
-      console.log(detections)
+      .withFaceExpressions();
+    console.log(detections);
+    // Display elements inside canvas
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    // Clear canvas before drawing
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    // Draw detections on canvas
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
   }, 100);
 });
