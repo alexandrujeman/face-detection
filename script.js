07d4f1c139cb481a5e80d9881353290542d0b26a
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-23 12:41:13 +0200

    Add video stream from webcam

---
+++ script.js
@@ -0,0 +1,15 @@
+const video = document.getElementById("video");
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => {
+      video.srcObject = stream;
+    },
+    err => {
+      console.error(err);
+    }
+  );
+}
+
+startVideo()
