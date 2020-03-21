9761718903e71ff22e7b7c34fd0e3c7c8683a7fb
alexjeman <alexandrujeman.ja@gmail.com>
2020-03-22 00:35:38 +0200

    Test

---
+++ video_demo_detectron2/main.py
@@ -0,0 +1,34 @@
+import detectron2
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import Visualizer
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+from detectron2 import model_zoo
+import random
+import cv2
+import numpy as np
+import torch
+import torchvision
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+print(torch.cuda.get_device_name(0))
+
+im = cv2.imread("./video_demo_detectron2/input.jpg")
+# cv2.imshow("test", im)
+# cv2.waitKey(0)
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+outputs["instances"].pred_classes
+outputs["instances"].pred_boxes
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imshow("preview", v.get_image()[:, :, ::-1])
+cv2.waitKey(0)
