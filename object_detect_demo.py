fbcf291f31239a3ae5644d823c7645a0c73e17ef
alexjeman <alexandrujeman.ja@gmail.com>
2020-03-21 13:57:17 +0200

    increase window scale

--- object_detect_demo.py
+++ object_detect_demo.py
@@ -22,7 +22,8 @@ dataset_dicts = DatasetCatalog.get("fruits_nuts")
 for d in random.sample(dataset_dicts, 3):
     img = cv2.imread(d["file_name"])
     visualizer = Visualizer(
-        img[:, :, ::-1], metadata=fruits_nuts_metadata, scale=0.5)
+        img[:, :, ::-1], metadata=fruits_nuts_metadata, scale=0.8
+    )
     vis = visualizer.draw_dataset_dict(d)
     cv2.imshow("preview", vis.get_image()[:, :, ::-1])
     cv2.waitKey(0)
